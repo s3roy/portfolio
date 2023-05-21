a4965742bfb519f9a59430f23b1168ca844f2197
Souvik <souvikroy1999ab@gmail.com>
2023-05-22 01:30:39 +0530

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react';
+
+import Layout from './Layout';
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+      <Layout className="p-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy;All Rights Reserved.</span>
+        <div className="flex items-center">
+          Build With
+          <span className="text-primary text-2xl px-1">&#9825;</span>
+          &nbsp;
+          <Link
+            href="#"
+            className="underline underline-offset-2"
+            target={'_blank'}
+          >
+            Nextjs
+          </Link>
+        </div>
+        <Link
+          href="#"
+          className="underline underline-offset-2"
+          target={'_blank'}
+        >
+          Say Hello
+        </Link>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
