be9526f0df82f808f9bd78c2177d16c95ae5c789
Souvik <souvikroy1999ab@gmail.com>
2023-05-22 00:47:02 +0530

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-inter)', ...fontFamily.sans],
+      },
+      colors: {
+        dark: '#1b1b1b',
+        light: '#f5f5f5',
+        primary: '#B63E96', // 240,86,199
+        primaryDark: '#58E6D9', // 80,230,217
+      },
+    },
   },
   plugins: [],
 };
