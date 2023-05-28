5f19247efd3dd2c86af656ffce3ddb83e23a8b74
Souvik <souvikroy1999ab@gmail.com>
2023-05-28 18:26:25 +0530

    linting fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 import Head from 'next/head';
 import Image from 'next/image';
