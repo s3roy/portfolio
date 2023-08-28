8f79b1a34c4b07b2d920aba1fc4f4acb4f6f145d
Souvik <souvikroy1999ab@gmail.com>
2023-08-28 23:31:23 +0530

    added experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -54,28 +54,36 @@ const Experience = () => {
         />
         <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
-            position={'Product Solution Engineer'}
-            company={'DemandFarm'}
+            position={'Software Development Engineer'}
+            company={'Onato'}
             companyLink={''}
-            time={'July,2022-March,2022'}
+            time={'April,2023-Present'}
             address={'Pune,India'}
-            work={undefined}
+            work={`Designed and maintained a robust Next.js application for the company's internal dashboard, leveraging React Hooks and
+Redux toolkit for state management, enhancing data accessibility, and improving decision-making processes.
+Leveraged Tailwind CSS and Material UI to create a responsive and user-friendly interface, adhering to modern design
+principles and increasing user engagement by 20%.
+Constructed an internal chat application for customer support, improving response times by 30% and boosting customer
+satisfaction by 15%.`}
           />
           <Details
             position={'Product Solution Engineer'}
             company={'DemandFarm'}
             companyLink={''}
-            time={'July,2022-March,2022'}
+            time={'July,2022-March,2023'}
             address={'Pune,India'}
-            work={undefined}
-          />
-          <Details
-            position={'Product Solution Engineer'}
-            company={'DemandFarm'}
-            companyLink={''}
-            time={'July,2022-March,2022'}
-            address={'Pune,India'}
-            work={undefined}
+            work={`Designed a Node.js-based CLI tool, integrating MySQL, Axios, Yargs, and Console.Table to retrieve and display user
+metrics, enhancing system integration insights.
+Launched a real-time Slack bot via the Slack API, driving a 20% error reduction and optimizing productivity by 8%
+through immediate error messages and login alerts.
+Streamlined processes to eliminate manual checks, resulting in a time-saving of 24 hours weekly.
+Led a two-way integration between KAM and external CRM systems using TypeScript, Node.js, and AWS webhooks,
+streamlining database operations and data synchronization.
+Decreased database errors by 80% and reduced database calls by 43% by implementing Sequelize ORM, ensuring robust
+and efficient data management.
+Developed a user-centric REST API with Spring Boot and PostgreSQL for consistent user data management and
+designed an intuitive admin interface using Next.js, React, and Chakra UI for enhanced user navigation and experience.
+Optimized data lookup by introducing an infinite scroll feature, leading to a 20% speed-up in user search activities.`}
           />
         </ul>
       </div>
